7c71fbd7aad349337ea0b9bcf3e236c4646d5d8f
Geilton Xavier <geilton@live.com>
2016-08-23 21:59:13 -0300

    Add view restaurants

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :restaurants
+
+  root 'restaurants#index'
 end
