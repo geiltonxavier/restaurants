0db15eb4517cc8e6b60c6c6d12a140da01b5dfd4
Geilton Xavier <geilton@live.com>
2016-08-23 22:56:27 -0300

    add view jsbuilder for return comments

---
+++ app/assets/javascripts/components/comment_list.js.jsx
@@ -0,0 +1,11 @@
+var CommentList = React.createClass({
+
+	render: function(){
+		return (
+			<div>
+				{JSON.parse(this.props.comments).map(function(comment){
+					return <Comment key={comment.id} rank={comment.rank} body={comment.body} author={comment.author} />;
+				})}
+			</div>);
+	}
+});
